ecfd93ba6cd4b9c053a65cba255c3da94c54da7d
najibu <ns.najibu@gmail.com>
2016-08-22 19:07:46 +0100

    Added navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,10 @@ class SessionsController < ApplicationController
 		session[:user_id] = user.id
 		redirect_to root_url, notice: "SIGNED IN" 
 	end
+
+	def destroy
+		session[:user_id] = nil 
+		session[:amniauth] = nil 
+		redirect_to root_url, notice: "SIGNED OUT"
+	end
 end
